e1b434c1d395262d6eac9b0da0947834a0988c92
virtualheet <heetvavadiya456@gmail.com>
2025-08-01 10:15:43 +0530

    Update: updateFee Update: updateOwner

--- contracts/Scratch.sol
+++ contracts/Scratch.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-
 contract Scratch {
     event PaymentSent(
         address indexed sender,
@@ -14,36 +13,39 @@ contract Scratch {
         bytes32 indexed paymentId
     );
 
+    uint256 public platformFee = 0; // for now
     uint256 private paymentCounter;
 
     address public owner;
 
-     function sendPayment(
+    function sendPayment(
         address payable _recipient,
         string calldata _message
     ) external payable nonReentrant {
         require(msg.value > 0, "Payment must be greater than 0");
         require(_recipient != address(0), "Invalid recipient");
         require(_recipient != msg.sender, "Cannot pay yourself");
-        
-        uint256 fee = (msg.value) / 100000;
+
+        uint256 fee = (msg.value * platformFee) / 100000;
         uint256 netAmount = msg.value - fee;
-        
+
         paymentCounter++;
-        bytes32 paymentId = keccak256(abi.encodePacked(
-            msg.sender,
-            _recipient,
-            msg.value,
-            block.timestamp,
-            paymentCounter
-        ));
-        
+        bytes32 paymentId = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                _recipient,
+                msg.value,
+                block.timestamp,
+                paymentCounter
+            )
+        );
+
         _recipient.transfer(netAmount);
-        
+
         if (fee > 0 && owner != address(0)) {
             payable(owner).transfer(fee);
         }
-        
+
         emit PaymentSent(
             msg.sender,
             _recipient,
@@ -53,10 +55,15 @@ contract Scratch {
             paymentId
         );
     }
-    
-    
-    
+
+    function updateFee(uint256 _newFee) external {
+        require(msg.sender == owner, "Only owner");
+        require(_newFee <= 1000, "Max 1% fee");
+        platformFee = _newFee;
+    }
+
+    function updateOwner(address _newOwner) external {
+        require(msg.sender == owner, "Only owner");
+        owner = _newOwner;
     }
 }
-
-
