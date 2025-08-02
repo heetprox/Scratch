ca0c89aeffcb1b0b1c6850e46b243dcd4f5c16a3
virtualheet <heetvavadiya456@gmail.com>
2025-08-02 23:33:31 +0530

    UI MY SKILLS STARTS

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,16 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./fonts.css";
 import { Web3Provider } from "../context/Provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -23,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`navigator-hand antialiased`}
       >
         <Web3Provider>
         {children}
