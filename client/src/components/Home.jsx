bb59bf178a31f7abab1b6c57b79197d9f376ea61
virtualheet <heetvavadiya456@gmail.com>
2025-08-06 18:31:15 +0530

    card

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -25,14 +25,41 @@ const Home = () => {
           </div>
         </Text>
 
-        <div className="flex absolute z-0 inset-0 w-full h-full justify-center flex-col items-center">
-          <Image
-            src="/bg-logo-2.svg"
-            width={400}
-            height={400}
-            alt='bg'
-            className='h-[30vh] md:h-[40vw] w-auto '
-          />
+        <div className="flex absolute z-0 inset-0 w-full h-full justify-end flex-col items-center md:items-end"
+        style={{
+              padding: "0 clamp(1rem, 2vw, 40rem)",
+        }}
+        >
+
+          <div className="flex flex-col bg-gradient-to-tl from-neutral-700/90 via-black to-neutral-400/60 w-[90%] md:w-[60%] h-[60vh] md:h-[35vw] border-t-4 border-x-4 border-white/60 rounded-t-[20vw] md:rounded-t-[6vw]"
+            style={{
+              padding: "clamp(1rem, 2vw, 40rem)",
+              gap: "clamp(1rem, 1vw, 40rem)",
+            }}
+          >
+            <div className="h-[30vh] md:h-[8.5vw]"
+            >
+              <Image
+                src="/bg-logo-2.svg"
+                width={400}
+                height={400}
+                alt='bg'
+                className='select-none rounded-l-full w-fit h-full '
+              />
+            </div> 
+            <div className="h-[30vh] md:h-[8.5vw]"
+            >
+              <Image
+                src="/heet.jpg"
+                width={400}
+                height={400}
+                alt='bg'
+                className='select-none rounded-full border-4 border-white/90 w-fit h-full '
+              />
+            </div>
+            
+          </div>
+
         </div>
         {/* <Text
           delay={0.6}
