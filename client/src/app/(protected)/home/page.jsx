b401e597a3e42a442bafcee1700261f7efbccf3c
virtualheet <heetvavadiya456@gmail.com>
2025-08-17 01:11:49 +0530

    bar.jsx

--- client/src/app/(protected)/home/page.jsx
+++ client/src/app/(protected)/home/page.jsx
@@ -1,8 +1,10 @@
+import Bar from '@/components/home/Bar'
 import React from 'react'
 
 const page = () => {
   return (
     <div className='w-full h-full bg-white min-h-screen'>
+        <Bar />
 
     </div>
   )
