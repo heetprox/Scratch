548e29d2d9109d048d7d487932a25432634796c1
virtualheet <heetvavadiya456@gmail.com>
2025-08-21 19:36:40 +0530

    style: adjust post container width for better layout

--- client/src/app/(protected)/v/page.jsx
+++ client/src/app/(protected)/v/page.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='w-full h-full flex justify-center bg-white min-h-screen'>
-      <div className="w-[30vw] mt-10">
+      <div className="w-[35vw] mt-10">
         <Post />
       </div>
     </div>
